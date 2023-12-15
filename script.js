999729bf1414af34a16a2d427e26b5474b4abb0d
Zytheta <112493405+Zytheta@users.noreply.github.com>
2023-12-15 16:16:52 +0900

    Added some books in json

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Fetching data from the books.json file
+fetch('./data/books.json')
+  .then(response => response.json())
+  .then(data => {
+    // Data from the JSON file is available here
+    const myBooks = data;
+    console.log(myBooks); // Displays the books array in the console
+
+    // Use myBooks as needed (e.g., display titles, manipulate data, etc.)
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
 const myLibrary = [];
 
 function Book() {
@@ -7,3 +21,16 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+// Function to fetch JSON data from the books json file
+async function fetchBooks() {
+    const response = await fetch('books.json');
+    const data = await response.json();
+    return data;
+  }
+  
+  // Function to filter books by genre
+  function filterBooksByGenre(books, genre) {
+    return books.filter(book => book.genre === genre);
+  }
+  
